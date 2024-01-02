5d0bbd5a431b449061c883aa17511a2e31036307
Gaspard Martet <gaspard.martet@icm-institute.org>
2024-01-02 17:05:40 +0100

    rajout du numéro de condition

--- R/ISI.R
+++ R/ISI.R
@@ -116,6 +116,7 @@ ISI <- function(
     start_TW <- info.cond$start_TW[j]
     end_TW <- info.cond$end_TW[j]
 
+    cond.nb <- guideline$ConditionNumber[i]
     expgrp <- guideline$ExperimentalGroup[j]
     expcond <- guideline$ExperimentalCondition[j]
     TeX <- info.cond$TeX[j]
